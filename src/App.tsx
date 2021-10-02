8f1e2d42081f527bbb492dbe3be394ce9888b389
Hygor Dias <hygor.christian@gmail.com>
2021-10-02 16:19:25 -0300

    adicionando fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import GoogleFonts, { Font } from "react-google-font-loader";
 import "./styles/global.scss";
 
+const fonts: Font[] = [
+  { font: "Caveat", weights: [400] },
+  { font: "Caveat Brush", weights: [400] },
+];
+
 function App() {
-  return <div className="App">rick and mortys</div>;
+  return (
+    <>
+      <GoogleFonts fonts={fonts} />
+      <h1>Rick and Morty</h1>
+    </>
+  );
 }
 
 export default App;
