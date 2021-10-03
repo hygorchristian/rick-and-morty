461386703dfcb5e9ff3e41e1cc884fcd3d026868
Hygor Dias <hygor.christian@gmail.com>
2021-10-03 15:58:28 -0300

    adicionando sass e configurando estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./styles/global.scss";
 
 function App() {
-  return <div className="App">rick and morty</div>;
+  return <div className="App">rick and mortys</div>;
 }
 
 export default App;
